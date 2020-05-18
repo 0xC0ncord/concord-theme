27ac62382babc5fa683001643bd0f6b4452ecf67
0xC0ncord <concord@fuwafuwatime.moe>
2020-05-17 22:22:40 -0400

    Initial commit

---
+++ assets/js/subtitle.js
@@ -0,0 +1,12 @@
+function randomize() {
+    subtitles = Array(
+        "Cool subtitle pending.",
+        "Make sure your servers always brush their teeth at least twice a day!",
+        "Coming soon to an Electron app near y-<br><span class='code'>FATAL ERROR: CALL_AND_RETRY_LAST Allocation failed - JavaScript heap out of memory</span>",
+        "Have you made a backup today?",
+        "My secret ingredient is undefined behavior.",
+        "<em>thicc</em> clients"
+    );
+    document.getElementById("subtitle").innerHTML = subtitles[Math.floor(Math.random() * subtitles.length)];
+}
+randomize();
