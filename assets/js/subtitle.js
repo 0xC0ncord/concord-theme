8ec734ddd13ff2ddbdeb8b89841343665587e7cc
0xC0ncord <concord@fuwafuwatime.moe>
2020-07-18 22:45:35 -0400

    Add new subtitle

--- assets/js/subtitle.js
+++ assets/js/subtitle.js
@@ -5,7 +5,8 @@ function randomize() {
         "Coming soon to an Electron app near y-<br><span class='code'>FATAL ERROR: CALL_AND_RETRY_LAST Allocation failed - JavaScript heap out of memory</span>",
         "Have you made a backup today?",
         "My secret ingredient is undefined behavior.",
-        "<em>thicc</em> clients"
+        "<em>thicc</em> clients",
+        "<span class='code'>setcap</span> > <span class='code'>suid</span> > <span class='code'>USER_NS</span>"
     );
     document.getElementById("subtitle").innerHTML = subtitles[Math.floor(Math.random() * subtitles.length)];
 }
