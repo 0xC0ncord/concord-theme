c517dad90da6c0477ba9867601f6d4afe46ade45
0xC0ncord <me@concord.sh>
2021-10-03 00:16:07 -0400

    assets: remove inline code class from subtitles

--- assets/js/subtitle.js
+++ assets/js/subtitle.js
@@ -2,11 +2,11 @@ function randomize() {
     subtitles = Array(
         "Cool subtitle pending.",
         "Make sure your servers always brush their teeth at least twice a day!",
-        "Coming soon to an Electron app near y-<br><span class='code'>FATAL ERROR: CALL_AND_RETRY_LAST Allocation failed - JavaScript heap out of memory</span>",
+        "Coming soon to an Electron app near y-<br>FATAL ERROR: CALL_AND_RETRY_LAST Allocation failed - JavaScript heap out of memory",
         "Have you made a backup today?",
         "My secret ingredient is undefined behavior.",
         "<em>thicc</em> clients",
-        "<span class='code'>setcap</span> > <span class='code'>suid</span> > <span class='code'>USER_NS</span>"
+        "setcap > suid > USER_NS"
     );
     document.getElementById("subtitle").innerHTML = subtitles[Math.floor(Math.random() * subtitles.length)];
 }
